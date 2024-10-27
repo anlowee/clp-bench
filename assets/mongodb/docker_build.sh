3aa5606cc167b3cccc8200c731de8edd9c133f5a
Xiaochong Wei <xiaochong.wei@mail.utoronto.ca>
2024-10-27 08:37:30 -0400

    :construction: wip

---
+++ assets/mongodb/docker_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -u
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+container_name="mongodb-clp-bench"
+
+docker build \
+	-t "$container_name" \
+	"$script_dir" \
+	--file "$script_dir"/Dockerfile
