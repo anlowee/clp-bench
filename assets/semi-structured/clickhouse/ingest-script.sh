c11f4912d91a307aff6e1f5832e1dfba25befbb3
Xiaochong Wei <xiaochong.wei@mail.utoronto.ca>
2024-11-08 14:14:19 -0500

    :lipstick: wrapped the lines in clickhouse assets

--- assets/semi-structured/clickhouse/ingest-script.sh
+++ assets/semi-structured/clickhouse/ingest-script.sh
@@ -2,4 +2,8 @@
 
 collection_name=clickhouse_clp_bench
 
-clickhouse-client -t --max_threads 1 --query "INSERT INTO ${collection_name} FROM INFILE '$1' FORMAT JSONAsString" >/dev/null 2>&1
+clickhouse-client \
+    -t \
+    --max_threads 1 \
+    --query "INSERT INTO ${collection_name} FROM INFILE '$1' FORMAT JSONAsString" \
+    >/dev/null 2>&1
--- assets/semi-structured/clickhouse/measure-compressed-size-script.sh
+++ assets/semi-structured/clickhouse/measure-compressed-size-script.sh
@@ -2,4 +2,5 @@
 
 collection_name=clickhouse_clp_bench
 
-clickhouse-client --query "SELECT SUM(bytes) FROM system.parts WHERE active AND table = '${collection_name}'"
+clickhouse-client \
+    --query "SELECT SUM(bytes) FROM system.parts WHERE active AND table = '${collection_name}'"
